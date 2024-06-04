9f52f6079c4a793e2c6da64cca7430cfc929aedc
L-Karimi <luciahkei101@gmail.com>
2024-06-04 23:54:35 +0300

    connected app to MOngoDB using Mongoose

--- server.js
+++ server.js
@@ -12,3 +12,15 @@ const PORT =process.env.PORT || 3000;
 app.listen(PORT,()=>{
   console.log("Server is running on ${PORT}");
 });
+
+
+const dotenv=require("dotenv")
+dotenc.config();
+const DATABASE_URL =process.env.DATABASE_URL;
+mongoose.connect(DATABASE_URL,{useNewParser:true,useUnifiedTopology:true})
+.then(()=>{
+  console.log("Server is connected to db")
+})
+.catchError((err)=>{
+  console.log("Error connecting to DB:",error)
+});
