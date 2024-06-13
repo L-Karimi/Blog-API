add5f4c285c98b4e835d7cb0799ddc555c67407e
L-Karimi <luciahkei101@gmail.com>
2024-06-13 22:19:13 +0300

    modified files

--- Comment.js
+++
@@ -1,9 +0,0 @@
-const mongoose =require('mongose')
-const commentSchema= new mongoose.schema({
-    commenter:String,
-    text:String,
-    blogPost:{type:blogPostSchema.objectId,ref:'BlogPost'},
-    creationDate:{type:Date,default:Date.now},
-})
-module.exports=mongoose.model
-{'Comment',commentSchema}
